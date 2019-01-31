8470e851217fc8524afb93201b41df357ae31f66
A Groshek <amy@amygroshek.com>
2019-01-30 22:25:12 -0600

    who we are section and custom botton type

--- themes/vanilla-bootstrap-hugo-theme/static/js/dnd-custom.js
+++ themes/vanilla-bootstrap-hugo-theme/static/js/dnd-custom.js
@@ -1,25 +1,26 @@
 (function($) {
     var jQuery = $.noConflict(true);
-    // From there on, window.jQuery and window.$ are undefined.
     var $ = jQuery;
-    // Do something with the local jQuery and $...
+
+    function checkScroll(y) {
+      if (y <= 0) {
+          $('body.home').addClass('scroll-top');
+      } else {
+          $('body.home').removeClass('scroll-top');
+      }
+    }
     $( document ).ready(function() {
         // console.log( "ready!" );
         // If on the home page, add scrollY tracking class to body tag.
         if ($('body.home').length >= 1) {
-          // console.log('On home page.');
-
-          // $('body.home').addClass('scroll-top');
           // When the window is scrolled, check scroll position.
           $( window ).scroll(function() {
               var t = $(window).scrollTop();
-              // console.log(t);
-              if (t <= 0) {
-                  $('body.home').addClass('scroll-top');
-              } else {
-                  $('body.home').removeClass('scroll-top');
-              }
+              checkScroll(t);
           });
         }
+        // Check on page load as well.
+        var t = $(window).scrollTop();
+        checkScroll(t);
     });
 })(jQuery);
