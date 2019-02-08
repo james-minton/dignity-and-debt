ae82e499e5cb9b108678e92582a251b855f05870
A Groshek <amy@amygroshek.com>
2019-02-08 10:31:54 -0600

    Work on about page

--- themes/vanilla-bootstrap-hugo-theme/static/js/dnd-custom.js
+++ themes/vanilla-bootstrap-hugo-theme/static/js/dnd-custom.js
@@ -22,5 +22,30 @@
         // Check on page load as well.
         var t = $(window).scrollTop();
         checkScroll(t);
+
+        // Handle bio modals
+        if ($('button').length >= 1) {
+          // console.log('exists');
+          $('.btn-bio').click(function(e) {
+            var $button = $(e.target);
+            e.preventDefault();
+            console.log('clicked');
+            // Get name, title, bio, and image
+            var parent = $button.parent();
+            console.log(parent);
+            var name = $button.parent().siblings('.name').text();
+            console.log('name = ' + name);
+            var title = $button.parent().siblings('.title').text();
+            console.log('title = ' + title);
+            var bio = $button.parent().siblings('.bio').html();
+            console.log('bio = ' + bio);
+            var image = $button.parent().siblings('.pic').html();
+            console.log('image = ' + image);
+            // Set contents
+            $('#modalLongTitle').text(name);
+            $('#peopleBioModal .modal-body').html(title + bio + image);
+            $('#peopleBioModal').modal('show');
+          });
+        }
     });
 })(jQuery);
